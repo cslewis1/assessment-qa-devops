f79ce25e2f322df4ae228032c669447ebc6da9d8
Charneaka Lewis <clewis20@ggc.edu>
2023-05-15 00:11:53 -0400

    adding automated test

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -16,15 +16,25 @@ describe("Duel Duo tests", () => {
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
 
-  test("Check that clicking the Draw button hides the button by adding the class hide", async () => {
+  test("Check that clicking the Draw button Draw button displays the div with id = choices", async () => {
     //navigate to the web app
     await driver.get("http://localhost:8000");
     //Find the draw button and click it
     await driver.findElement(By.id("draw")).click()
-    //Check to see if the class with the name of 'hide' is added.
-    const displayRobots = await driver.wait(until.elementLocated(By.css("#container div")), 1000)
-    expect(await displayRobots.getText()).toBe("")
+    //Check to see if the div with the name of 'choices' is added.
+    const displayRobots = await driver.wait(until.elementLocated(By.id("choices")), 1000)
+    expect(await displayRobots.isDisplayed())
   })
 
-
+  test("Check that clicking an “Add to Duo” button displays the div with id = player-duo", async () => {
+    //navigate to the web app
+    await driver.get("http://localhost:8000");
+     //Find the draw button and click it
+     await driver.findElement(By.id("draw")).click()
+    //Find the Add to duo button and click it
+    await driver.findElement(By.className("bot-btn")).click()
+    //Check to see if the div with the id of 'player-duo' is added.
+    const playerRobots = await driver.wait(until.elementLocated(By.id("player-duo")), 1000)
+    expect(await playerRobots.isDisplayed())
+  })
 }); 
